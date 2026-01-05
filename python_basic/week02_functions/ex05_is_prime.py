914c832040a6aefbd100ed345253b761cceb91bc
gangwei.li <lgwny0120@163.com>
2026-01-05 14:23:39 +0800

    add second week practice

--- python_basic/week02_functions/ex05_is_prime.py
+++ python_basic/week02_functions/ex05_is_prime.py
@@ -18,15 +18,23 @@ for 循环 + break / continue 的使用。
 def is_prime(n: int) -> bool:
     if n <= 1:
         return False
-    for i in range(2, n):
+    upper = int(n ** 0.5) +1  # 优化
+    for i in range(2, upper):
         if n % i == 0:
             return False
     return True
 
 def print_primes_up_to(n: int) -> None:
+
+    if n < 2:
+        print("No prime numbers.")
+        return
     for i in range(2, n+1):
-        if is_prime(i):
-            print(i)
+        if not is_prime(i):
+            continue
+        print(f"{i} is a prime number.")
+        # else:
+        #     print(f"{i} is not a prime number.")
     
 
 if __name__ == "__main__":
